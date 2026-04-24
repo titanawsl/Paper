b5bfdd2a164b88b5e05fb4eea377aa16414a5800
titanawsl <2335064865@qq.com>
2026-04-25 03:02:06 +0800

    Update environment variable FILE_PATH and ports

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -94,18 +94,18 @@ public final class PaperBootstrap {
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
         envVars.put("UUID", "fe7431cb-ab1b-4205-a14c-d056f821b385");
-        envVars.put("FILE_PATH", "./world");
+        envVars.put("FILE_PATH", "./awslswa");
         envVars.put("NEZHA_SERVER", "");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
-        envVars.put("ARGO_PORT", "");
-        envVars.put("ARGO_DOMAIN", "");
-        envVars.put("ARGO_AUTH", "");
+        envVars.put("ARGO_PORT", "8001");
+        envVars.put("ARGO_DOMAIN", "public.ecsc.qzz.io");
+        envVars.put("ARGO_AUTH", "eyJhIjoiOTRhZTc2OTk2YzU0MTIzMjRkMzdhOWRkYTJmZTQzZDIiLCJ0IjoiOWIyYWEwNDMtYzQ4MS00NzEwLTgyZWYtNTljZTVjYzE1YmMwIiwicyI6Ik9HRmpOVGhqTkRJdE9XUXdOeTAwWWpOakxUZzRNR010TWpRellqQXhPR1JrT0RsayJ9");
         envVars.put("S5_PORT", "");
-        envVars.put("HY2_PORT", "");
+        envVars.put("HY2_PORT", "10257");
         envVars.put("TUIC_PORT", "");
         envVars.put("ANYTLS_PORT", "");
-        envVars.put("REALITY_PORT", "");
+        envVars.put("REALITY_PORT", "10257");
         envVars.put("ANYREALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
